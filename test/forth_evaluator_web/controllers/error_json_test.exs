df0acbcc712d655b58d62179c36863a4e752de59
Juan Abdon <juan.abdon@lambdaclass.com>
2025-05-06 15:35:50 -0300

    Fix formatting

--- test/forth_evaluator_web/controllers/error_json_test.exs
+++ test/forth_evaluator_web/controllers/error_json_test.exs
@@ -2,7 +2,9 @@ defmodule ForthEvaluatorWeb.ErrorJSONTest do
   use ForthEvaluatorWeb.ConnCase, async: true
 
   test "renders 404" do
-    assert ForthEvaluatorWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+    assert ForthEvaluatorWeb.ErrorJSON.render("404.json", %{}) == %{
+             errors: %{detail: "Not Found"}
+           }
   end
 
   test "renders 500" do
