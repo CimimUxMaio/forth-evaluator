c14ffd93647ed9489ab9d6ebf83bbd85e8d37410
Juan Abdon <juan.abdon@lambdaclass.com>
2025-05-23 13:16:44 -0300

    Improve parsers comments

--- lib/forth_evaluator/parser.ex
+++ lib/forth_evaluator/parser.ex
@@ -1,4 +1,35 @@
 defmodule ForthEvaluator.Parser do
+  @moduledoc """
+  This modules defines the `program_parser` and a function that executes it
+  with a given program string (`parse_program`).
+
+  The program parser is composed by many different, smaller and simpler parsers.
+
+  Each parser is a function that takes a list of words as input and outputs
+  a match tuple of the form `{ok, {match, remainder}}`. 
+
+  The match portion is the matching value produced by the matching words in the 
+  list (from the begining) and the parser's internal logic.
+
+  The remainder portion is the list of words from the input that the parser
+  failed to match.
+
+  In the end, given a list of words N, a parser may produce a match for the first
+  M words, producing a remainder of N - M words.
+
+  The ok portion indicates whether the parser failed or not, it can be either `:ok`
+  or `:error`.
+
+  For example, the number parser parses the first word of the list into a stack 
+  push operation:
+
+    iex> number_parser(["1", "2", "3"])
+    {:ok, {{:stack_op, :push, [1]}, ["2", "3"]}} # => Remainder: ["2", "3"]
+
+    iex> number_parser(["hello", "world"])
+    {:error, "expected number but found nothing"}
+  """
+
   alias ForthEvaluator.Parser.Combinators
 
   @type word :: String.t()
@@ -45,10 +76,8 @@ defmodule ForthEvaluator.Parser do
     Combinators.repeat_once(&operation_parser/1) |> apply([words])
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches only numbers and, if successful, returns a tuple 
-  # with a "push" `stack_op` for the matching number and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first word of the list of words into a stack push operation.
+  # If the word is a numeric value, it succeeds. Otherwise it returns an error tuple.
   @spec number_parser(words :: [word]) :: {:ok, {stack_op, [word]}} | {:error, String.t()}
   defp number_parser(words)
 
@@ -61,10 +90,8 @@ defmodule ForthEvaluator.Parser do
     end
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches all predefined stack operations and, if successful, 
-  # returns a tuple with the matching `stack_op` and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first word of the list of words into a stack predefined operation.
+  # If the word matches a predefined command, it succeeds. Otherwise it returns an error tuple.
   defp predefined_stack_op_parser(words)
 
   defp predefined_stack_op_parser([]), do: {:error, "expected operation but found nothing"}
@@ -84,40 +111,37 @@ defmodule ForthEvaluator.Parser do
     end
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches any operation (either stack opertaion or dictionary operations)
-  # and, if successful, returns a tuple with the matching operation tuple and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first N words of the list of words into any operation.
+  # If the words match either a stack operation or a dictionary operation, it succeeds. 
+  # Otherwise it returns an error tuple.
   defp operation_parser(words) do
     [&stack_op_parser/1, &dictionary_op_parser/1]
     |> Combinators.any()
     |> apply([words])
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches any stack operation (numbers and predefined operations)
-  # and, if successful, returns a tuple with the matching `stack_op` and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first word of the list of words into any stack operation.
+  # If the word matches either a numeric value or a predefined command, it succeeds. 
+  # Otherwise it returns an error tuple.
   defp stack_op_parser(words) do
     [&predefined_stack_op_parser/1, &number_parser/1]
     |> Combinators.any()
     |> apply([words])
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches any stack operation (numbers and predefined operations)
-  # and, if successful, returns a tuple with the matching `dictionary_op` and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first N words of the list of words into a dictionary operation.
+  # If the words match either a definition or an evaluation, it succeeds. 
+  # Otherwise it returns an error tuple.
   defp dictionary_op_parser(words) do
     [&definition_parser/1, &evaluation_parser/1]
     |> Combinators.any()
     |> apply([words])
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches new word definitions and, if successful, returns 
-  # a tuple with the matching `dictionary_op` and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first N words of the list of words into a dictionary definition
+  # operation.
+  # If the words match a definition structure, it succeeds. 
+  # Otherwise it returns an error tuple.
   def definition_parser(words) do
     colon_parser = Combinators.consume(&parse_match(":", &1))
     semicolon_parser = Combinators.consume(&parse_match(";", &1))
@@ -133,10 +157,9 @@ defmodule ForthEvaluator.Parser do
     end
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches any valid word that could be evaluated, if successful, returns 
-  # a tuple with the matching `dictionary_op` and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first word of the list of words into a dictionary evaluation operation.
+  # If the word matches with a valid name, it succeeds. 
+  # Otherwise it returns an error tuple.
   defp evaluation_parser(words) do
     case words |> name_parser() do
       {:ok, {name, remainder}} -> {:ok, {{:dictionary_op, :search, [name]}, remainder}}
@@ -151,20 +174,18 @@ defmodule ForthEvaluator.Parser do
     Regex.match?(~r/^[a-zA-Z_][a-zA-Z0-9_]*$/, name)
   end
 
-  # Parser that parses a program given as a list of words.
-  # This parser matches any valid word, if successful, returns 
-  # a tuple with the corresponding word as a string and the remaining 
-  # program that was not able to parse (an empty list if none).
+  # Parser that parses the first word of the list of words into a name.
+  # If the word matches with a valid name, it succeeds. 
+  # Otherwise it returns an error tuple.
   defp name_parser([]), do: {:error, "expected name but found nothing"}
 
   defp name_parser([head | tail]) do
     if valid_name?(head), do: {:ok, {head, tail}}, else: {:error, "invalid name '#{head}'"}
   end
 
-  # This function parses a program given as a list of words.
-  # It matches a given string (`text`) with the first word of the program.
-  # If successful, returns a tuple with the corresponding word as a 
-  # string and the remaining program that was not able to parse (an empty list if none).
+  # This function matches a given string (`text`) with the first word of a list of words.
+  # If the word mathces the input, is succeeds returning a parser success tuple.
+  # Otherwise, it fails, returning a parser failure tuple.
   defp parse_match(text, []), do: {:error, "expected '#{text}' but found nothing"}
 
   defp parse_match(text, [head | tail]) do
